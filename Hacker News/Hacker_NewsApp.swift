fc039a09ccf437964aa69041b44d601cd75e0560
U-ma-s <yuma.prodia4989@gmail.com>
2023-02-22 21:42:52 +0900

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by 杉下友麻 on 2023/02/22.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
