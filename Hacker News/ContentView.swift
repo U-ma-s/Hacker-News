1519abe013f7f9279e93277e9d00f6bcc655d46a
U-ma-s <yuma.prodia4989@gmail.com>
2023-02-23 13:12:33 +0900

    initial commit

--- Hacker News/ContentView.swift
+++ Hacker News/ContentView.swift
@@ -1,21 +1,27 @@
-//
-//  ContentView.swift
-//  Hacker News
-//
-//  Created by 杉下友麻 on 2023/02/22.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()//@Publishedに設定したプロパティのリスナー
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            //List(posts, rowContent: {post in Text(post.title)})
+            //List(posts){Text($0.title)}
+            List(networkManager.posts) {post in
+                HStack {
+                    Text(String(post.points))
+                    Text(post.title)
+                }
+                
+            }
+            
+            .navigationTitle("Hacker News")
         }
-        .padding()
+        .onAppear {//viewdidLoad()的な
+            self.networkManager.fetchData()
+        }
     }
 }
 
@@ -24,3 +30,10 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+//let posts = [
+//    Post(id: "1", title: "Hello"),
+//    Post(id: "2", title: "こんにちは"),
+//    Post(id: "3", title: "Bonjour")
+//]
